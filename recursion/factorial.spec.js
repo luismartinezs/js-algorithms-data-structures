342d0064b4d0d92f6b1cef23d2a92d5025be5fcb
luismartinezs <martinez.s.luis@gmail.com>
2020-07-10 17:22:14 +0200

    Factorial handles 0

--- recursion/factorial.spec.js
+++ recursion/factorial.spec.js
@@ -2,6 +2,7 @@ import factorial from './factorial.js'
 
 describe('factorial', () => {
   test('Return the factorial', () => {
+    expect(factorial(0)).toEqual(1)
     expect(factorial(1)).toEqual(1)
     expect(factorial(2)).toEqual(2)
     expect(factorial(4)).toEqual(24)
