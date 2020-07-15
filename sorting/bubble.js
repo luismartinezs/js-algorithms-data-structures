ef6da88c02adefdf7b634e7a0354f6444796943d
luismartinezs <martinez.s.luis@gmail.com>
2020-07-15 07:51:50 +0200

    Optimized bubble

--- sorting/bubble.js
+++ sorting/bubble.js
@@ -1,10 +1,14 @@
 function bubble (arr) {
+  let noSwap
   for (let i = arr.length - 1; i >= 0; i--) {
+    noSwap = true
     for (let j = 0; j <= i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         ;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+        noSwap = false
       }
     }
+    if (noSwap) break
   }
 
   return arr
--- sorting/bubble.spec.js
+++ sorting/bubble.spec.js
@@ -6,5 +6,7 @@ describe('bubble', () => {
     expect(bubble(arr)).toEqual([1, 2, 3])
     arr = [2, 34, 55, 6, 2, 100, 99]
     expect(bubble(arr)).toEqual([2, 2, 6, 34, 55, 99, 100])
+    arr = [8, 1, 2, 3, 4, 5, 6, 7]
+    expect(bubble(arr)).toEqual([1, 2, 3, 4, 5, 6, 7, 8])
   })
 })
