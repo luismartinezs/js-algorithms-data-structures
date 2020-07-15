13be8bda258b95aa0c4a61da04e55b37a8fe8572
luismartinezs <martinez.s.luis@gmail.com>
2020-07-15 09:04:02 +0200

    Second version of insertion sort

--- sorting/insertionSort.js
+++ sorting/insertionSort.js
@@ -1,4 +1,13 @@
-function insertionSort (arr) {
+function insertionSort1 (arr) {
+  function insert (arr, insertee, insertor) {
+    return [
+      ...arr.slice(0, insertor),
+      arr[insertee],
+      ...arr.slice(insertor, insertee),
+      ...arr.slice(insertee + 1)
+    ]
+  }
+
   let pointer
 
   for (let i = 1; i <= arr.length - 1; i++) {
@@ -11,15 +20,30 @@ function insertionSort (arr) {
       }
     }
 
-    arr = [
-      ...arr.slice(0, pointer),
-      arr[i],
-      ...arr.slice(pointer, i),
-      ...arr.slice(i + 1)
-    ]
+    if (i !== pointer) {
+      arr = insert(arr, i, pointer)
+    }
   }
 
   return arr
 }
 
-export default insertionSort
+function insertionSort2 (arr) {
+  for (let i = 1; i <= arr.length - 1; i++) {
+    const currentValue = arr[i]
+    let pointer = i
+
+    for (let j = i - 1; j >= 0 && arr[j] >= currentValue; j--) {
+      arr[j + 1] = arr[j]
+      pointer = j
+    }
+
+    if (pointer !== i) {
+      arr[pointer] = currentValue
+    }
+  }
+
+  return arr
+}
+
+export default insertionSort2
