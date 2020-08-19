490a7d65ed589a5bc4f41804814b392d5399862a
luismartinezs <martinez.s.luis@gmail.com>
2020-08-19 11:26:41 +0200

    Tabular Fibonacci

--- dynamic-programming/fib.js
+++ dynamic-programming/fib.js
@@ -1,16 +1,36 @@
 // memoized fibonacci
-function fib (n, memo = [undefined, 1, 1]) {
+// O(n)
+// Can give exceed call stack size (10000)
+function memoFib (n, memo = [undefined, 1, 1]) {
   if (memo[n] !== undefined) {
     return memo[n]
   }
   // if (n <= 2) return 1
-  const res = fib(n - 1, memo) + fib(n - 2, memo)
+  const res = memoFib(n - 1, memo) + memoFib(n - 2, memo)
   memo[n] = res
   return res
 }
 
 const cache = [undefined, 1, 1]
 
-fib(100, cache)
+memoFib(100, cache)
 
-console.log(cache[100])
+console.log(cache[10])
+
+// Tabulated fibonacci
+// O(n)
+// Better space complexity than memoized version
+// Cannot generate call stack size error
+function tabFib (n) {
+  if (n <= 2) {
+    return 1
+  }
+
+  const fibNums = [0, 1, 1]
+  for (let i = 3; i <= n; i++) {
+    fibNums[i] = fibNums[i - 1] + fibNums[i - 2]
+  }
+  return fibNums[n]
+}
+
+console.log(tabFib(10))
